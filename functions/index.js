ee282750468e86e37baf6614d460f579e510704f
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-08 18:47:32 -0400

    deployed api and database, and added links to prove they're working

--- functions/index.js
+++ functions/index.js
@@ -12,8 +12,13 @@ app.post ('/dogList', createNewDog)
 
 app.get ('/dogList', getAllDogs)
 
-app.listen(3013, () => {
-  console.log('Listening on http://localhost:3013...')})
+// app.listen(3013, () => {
+  //   console.log('Listening on http://localhost:3013...')})
 
+// http://127.0.0.1:5001/fur-ever-friend-api/us-central1/api/dogList
+// use this to run emulators for api
+
+// https://us-central1-fur-ever-friend-api.cloudfunctions.net/api/dogList
+// use this link to view deployed api
 
 export const api = functions.https.onRequest(app)
