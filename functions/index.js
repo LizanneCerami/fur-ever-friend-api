4c2d3578197b193474c57f6fbbbcb14dd97f126a
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-15 16:24:08 -0400

    fixed an error with index.js

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
-import { createNewDog, getAllDogs, updateDog } from './src/dogList.js'
+import { getAllDogs, updateDog } from './src/dogList.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.post ('/dogList', createNewDog)
+
 app.patch('/dogList/:dogId', updateDog);
 app.get ('/dogList', getAllDogs)
 
